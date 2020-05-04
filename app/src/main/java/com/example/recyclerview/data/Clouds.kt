56ef11beb05541d9fc5ff5920bffa8e465d87291
Michil <herm1t2142@gmail.com>
2020-05-04 18:32:44 +0900

    fix bug #1

--- app/src/main/java/com/example/recyclerview/data/Clouds.kt
+++
@@ -1,5 +0,0 @@
-package com.example.recyclerview.data
-
-data class Clouds(
-    val all: Int
-)
--- app/src/main/java/com/example/recyclerview/network/ApiFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.example.recyclerview.network
-
-import com.example.recyclerview.AppConstants
-
-object ApiFactory{
-
-    val weatherHolderApi: WeatherApi = RetrofitFactory.retrofit(AppConstants.WEATHER_BASE_URL)
-        .create(WeatherApi::class.java)
-}
