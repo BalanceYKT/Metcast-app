61669648e012e92adef896829b815feb314bb7f3
Michil <herm1t2142@gmail.com>
2020-05-07 21:07:12 +0900

    Add today forecast

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -21,35 +21,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // инициализация
-        // инициализация
+
+        // ------------------- TabHost -------------------
         tabHostT.setup()
 
-        // создаем вкладку и указываем тег
-
-        // создаем вкладку и указываем тег
         val tabSpec = tabHostT.newTabSpec("tag1")
         tabSpec.setIndicator("Today")
-        tabSpec.setContent(my_recycler_view.id)
+        tabSpec.setContent(today.id)
         tabHostT.addTab(tabSpec)
 
         val tabSpec2 = tabHostT.newTabSpec("tag2")
         tabSpec2.setIndicator("Tomorrow")
-        tabSpec2.setContent(my_recycler_view2.id)
+        tabSpec2.setContent(tomorrow.id)
         tabHostT.addTab(tabSpec2)
 
-        val tabSpec3 = tabHostT.newTabSpec("tag2")
+        val tabSpec3 = tabHostT.newTabSpec("tag3")
         tabSpec3.setIndicator("On week")
-        tabSpec3.setContent(my_recycler_view3.id)
+        tabSpec3.setContent(week.id)
         tabHostT.addTab(tabSpec3)
 
 
 
-        // -------------------network service ---------------------
+        // ------------------- network service ---------------------
         val weatherService = ApiFactory.weatherHolderApi
         weatherService.getWeather(YakutskCity, TemperatureUnit).enqueue(object : Callback<Forecast>{
             override fun onResponse(call: Call<Forecast>, response: Response<Forecast>) {
                 showData(response.body()!!)
+                showRecyclerData(response.body()!!)
                 d("asd", "Success" )
             }
 
@@ -61,8 +59,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun showData(Items: Forecast) {
-        my_recycler_view.apply {
+
+    private fun showData(Item: Forecast) {
+        todayTemperature.text = Item.list[0].main.temp.toString()
+        todayDescription.text = Item.list[0].weather[0].description
+        todayWind.text = Item.list[0].wind.speed.toString()
+        todayPressure.text = Item.list[0].main.pressure.toString()
+        todayHumidity.text = Item.list[0].main.humidity.toString()
+        todayImageViewWeather.setImageResource(R.drawable.ic_weather_sun)
+        val todayIcon = Item.list[0].weather[0].icon
+        showIcon(todayIcon)
+    }
+
+    private fun showRecyclerData(Items: Forecast) {
+        my_recycler_view1.apply {
             adapter = Adapter(Items)
             layoutManager = LinearLayoutManager(this@MainActivity)
             setHasFixedSize(true)
@@ -81,6 +91,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showIcon(Icon: String){
+        when (Icon){
+            "01d" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_clear_sky)
+            "01n" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_clear_sky)
+            "02d" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_few_cloud)
+            "02n" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_few_cloud)
+            "03d" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_scattered_clouds)
+            "03n" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_scattered_clouds)
+            "04d" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_broken_clouds)
+            "04n" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_broken_clouds)
+            "09d" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_shower_rain)
+            "09n" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_shower_rain)
+            "10d" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_rain)
+            "10n" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_rain)
+            "11n" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_thunderstorm)
+            "11d" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_thunderstorm)
+            "13d" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_snow)
+            "13n" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_snow)
+            "15d" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_mist)
+            "15n" -> todayImageViewWeather.setImageResource(R.drawable.ic_weather_mist)
+        }
+    }
 
 
 }
