ee854553d21e9cf027a78786da5648a44b7f729c
Michil <herm1t2142@gmail.com>
2020-05-14 18:23:34 +0900

    Replaced Retrofit.enqueue with Kotlin coroutines

--- app/src/main/java/com/example/recyclerview/network/WeatherApi.kt
+++ app/src/main/java/com/example/recyclerview/network/WeatherApi.kt
@@ -1,12 +1,12 @@
 package com.example.recyclerview.network
 
 import com.example.recyclerview.model.Forecast
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
 
     @GET("forecast")
-    fun getWeather(@Query("q") cityName: String, @Query("units") TemperatureUnit: String): Call<Forecast>
+    suspend fun getWeather(@Query("q") cityName: String, @Query("units") TemperatureUnit: String): Forecast
 }
